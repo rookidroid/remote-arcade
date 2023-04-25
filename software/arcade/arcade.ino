0a8775c42707289154ffbdd90d27c69d2de1880c
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-04-25 16:14:34 -0400

    Update arcade.ino

--- software/arcade/arcade.ino
+++ software/arcade/arcade.ino
@@ -65,12 +65,6 @@ const int udpPort = 1234;
 const int pwmFreq = 5000;
 const int pwmResolution = 8;
 
-// joystick values
-int valX = 0;
-int valY = 0;
-int valSw = 0;
-
-// old joystick values
 int js_up = 1;
 int js_down = 1;
 int js_left = 1;
@@ -80,17 +74,6 @@ int bt_up = 1;
 int bt_down = 1;
 int bt_left = 1;
 int bt_right = 1;
-
-// new joystick values
-int js_up_new = 1;
-int js_down_new = 1;
-int js_left_new = 1;
-int js_right_new = 1;
-
-int bt_up_new = 1;
-int bt_down_new = 1;
-int bt_left_new = 1;
-int bt_right_new = 1;
 
 void setup()
 {
@@ -133,29 +116,19 @@ void setup()
 
 void loop()
 {
-  js_up_new = digitalRead(JS_UP);
-  js_down_new = digitalRead(JS_DOWN);
-  js_left_new = digitalRead(JS_LEFT);
-  js_right_new = digitalRead(JS_RIGHT);
+  js_up = digitalRead(JS_UP);
+  js_down = digitalRead(JS_DOWN);
+  js_left = digitalRead(JS_LEFT);
+  js_right = digitalRead(JS_RIGHT);
 
-  bt_up_new = digitalRead(BT_UP);
-  bt_down_new = digitalRead(BT_DOWN);
-  bt_left_new = digitalRead(BT_LEFT);
-  bt_right_new = digitalRead(BT_RIGHT);
+  bt_up = digitalRead(BT_UP);
+  bt_down = digitalRead(BT_DOWN);
+  bt_left = digitalRead(BT_LEFT);
+  bt_right = digitalRead(BT_RIGHT);
 
-  if (js_up != js_up_new || js_down != js_down_new || js_left != js_left_new || js_right != js_right_new)
+  if ((js_up + js_down + js_left + js_right) != 4)
   {
-    js_up = js_up_new;
-    js_down = js_down_new;
-    js_left = js_left_new;
-    js_right = js_right_new;
-    if ((js_up + js_down + js_left + js_right) == 4)
-    {
-      udp.beginPacket(udpAddress, udpPort);
-      udp.printf("L0:R0:");
-      udp.endPacket();
-    }
-    else if ((js_up + js_down + js_left + js_right) == 2)
+    if ((js_up + js_down + js_left + js_right) == 2)
     {
       if (js_up == 0 && js_left == 0)
       {
@@ -216,98 +189,95 @@ void loop()
       }
     }
   }
-  else if (bt_up != bt_up_new || bt_down != bt_down_new|| bt_left != bt_left_new || bt_right != bt_right_new)
+  else if ((bt_up + bt_down + bt_left + bt_right) != 4)
   {
-    bt_up = bt_up_new;
-    bt_down = bt_down_new;
-    bt_left = bt_left_new;
-    bt_right = bt_right_new;
-    if ((bt_up+bt_down+bt_left+bt_right) == 4)
+    if ((bt_up + bt_down + bt_left + bt_right) == 2)
     {
-      udp.beginPacket(udpAddress, udpPort);
-      udp.printf("L0:R0:");
-      udp.endPacket();
-    }
-    else if ((bt_up+bt_down+bt_left+bt_right) == 2)
-    {
-        if (bt_up == 0 && bt_left == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L-128:R-255:");
-          udp.endPacket();
-        }
-        else if (bt_up == 0 && bt_right == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L-255:R-128:");
-          udp.endPacket();
-        }
-        else if (bt_down == 0 && bt_left == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L128:R255:");
-          udp.endPacket();
-        }
-        else if (bt_down == 0 && bt_right == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L255:R128:");
-          udp.endPacket();
-        }
-        else
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L0:R0:");
-          udp.endPacket();
-        }
+      if (bt_up == 0 && bt_left == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L-128:R-255:");
+        udp.endPacket();
+      }
+      else if (bt_up == 0 && bt_right == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L-255:R-128:");
+        udp.endPacket();
+      }
+      else if (bt_down == 0 && bt_left == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L128:R255:");
+        udp.endPacket();
+      }
+      else if (bt_down == 0 && bt_right == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L255:R128:");
+        udp.endPacket();
+      }
+      else
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L0:R0:");
+        udp.endPacket();
+      }
     }
     else
     {
-        if (bt_up == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L-255:R-255:");
-          udp.endPacket();
-        }
-        else if (bt_down == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L255:R255:");
-          udp.endPacket();
-        }
-        else if (bt_left == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L255:R-255:");
-          udp.endPacket();
-        }
-        else if (bt_right == 0)
-        {
-          udp.beginPacket(udpAddress, udpPort);
-          udp.printf("L-255:R255:");
-          udp.endPacket();
-        }
+      if (bt_up == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L-255:R-255:");
+        udp.endPacket();
+      }
+      else if (bt_down == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L255:R255:");
+        udp.endPacket();
+      }
+      else if (bt_left == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L255:R-255:");
+        udp.endPacket();
+      }
+      else if (bt_right == 0)
+      {
+        udp.beginPacket(udpAddress, udpPort);
+        udp.printf("L-255:R255:");
+        udp.endPacket();
+      }
     }
   }
+  else
+  {
+    udp.beginPacket(udpAddress, udpPort);
+    udp.printf("L0:R0:");
+    udp.endPacket();
+  }
+  delay(100);
 }
 
 void connectToWiFi(const char *ssid, const char *pwd)
 {
-    analogWrite(PIN_DISCONNECTED, 2);
-    analogWrite(PIN_CONNECTED, 0);
+  analogWrite(PIN_DISCONNECTED, 2);
+  analogWrite(PIN_CONNECTED, 0);
 
-    // delete old config
-    WiFi.disconnect(true);
+  // delete old config
+  WiFi.disconnect(true);
 
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print('.');
-      delay(500);
-    }
-    Serial.print("Connected! IP address: ");
-    Serial.println(WiFi.localIP());
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
 
-    analogWrite(PIN_DISCONNECTED, 0);
-    analogWrite(PIN_CONNECTED, 2);
+  analogWrite(PIN_DISCONNECTED, 0);
+  analogWrite(PIN_CONNECTED, 2);
 }
