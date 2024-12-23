c7ff7f677cc9bba776b88a3864da8a8c6087b58f
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-12-23 12:37:45 -0500

    use interrupt for the buttons

--- software/arcade_hexapod/arcade_hexapod.ino
+++ software/arcade_hexapod/arcade_hexapod.ino
@@ -85,8 +85,13 @@ int bt_left = 1;
 int bt_right = 1;
 int bt_special = 1;
 
-void setup()
-{
+int button_pressed = 0;
+
+void IRAM_ATTR isrButton() {
+  button_pressed++;
+}
+
+void setup() {
   // initilize hardware serial:
   Serial.begin(115200);
 
@@ -108,63 +113,115 @@ void setup()
   pinMode(LED_3, OUTPUT);
   pinMode(LED_4, OUTPUT);
 
+  attachInterrupt(JS_UP, isrButton, CHANGE);
+  attachInterrupt(JS_DOWN, isrButton, CHANGE);
+  attachInterrupt(JS_LEFT, isrButton, CHANGE);
+  attachInterrupt(JS_RIGHT, isrButton, CHANGE);
+
+  attachInterrupt(BT_UP, isrButton, CHANGE);
+  attachInterrupt(BT_DOWN, isrButton, CHANGE);
+  attachInterrupt(BT_LEFT, isrButton, CHANGE);
+  attachInterrupt(BT_RIGHT, isrButton, CHANGE);
+  attachInterrupt(BT_SPECIAL, isrButton, CHANGE);
+
   // blink LEDs
   analogWrite(LED_POWER, 2);
   analogWrite(LED_CONNECTED, 2);
+
+  // analogWrite(LED_1, 2);
+  // analogWrite(LED_2, 2);
+  // analogWrite(LED_3, 2);
+  // analogWrite(LED_4, 2);
 
   delay(1000);
 
   connectToWiFi(ssid, password);
 }
 
-void loop()
-{
-  js_up = digitalRead(JS_UP);
-  js_down = digitalRead(JS_DOWN);
-  js_left = digitalRead(JS_LEFT);
-  js_right = digitalRead(JS_RIGHT);
+void loop() {
+  if (button_pressed > 0) {
+    js_up = digitalRead(JS_UP);
+    js_down = digitalRead(JS_DOWN);
+    js_left = digitalRead(JS_LEFT);
+    js_right = digitalRead(JS_RIGHT);
 
-  bt_up = digitalRead(BT_UP);
-  bt_down = digitalRead(BT_DOWN);
-  bt_left = digitalRead(BT_LEFT);
-  bt_right = digitalRead(BT_RIGHT);
-  bt_special = digitalRead(BT_SPECIAL);
+    bt_up = digitalRead(BT_UP);
+    bt_down = digitalRead(BT_DOWN);
+    bt_left = digitalRead(BT_LEFT);
+    bt_right = digitalRead(BT_RIGHT);
+    bt_special = digitalRead(BT_SPECIAL);
 
-  if ((js_up + js_down + js_left + js_right) != 4)
-  {
-    if ((js_up + js_down + js_left + js_right) == 2)
-    {
-      if (js_up == 0 && js_left == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkl45:");
-        udp.endPacket();
+    if ((js_up + js_down + js_left + js_right) != 4) {
+      if ((js_up + js_down + js_left + js_right) == 2) {
+        if (js_up == 0 && js_left == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkl45:");
+          udp.endPacket();
+        } else if (js_up == 0 && js_right == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkr45:");
+          udp.endPacket();
+        } else if (js_down == 0 && js_left == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkl135:");
+          udp.endPacket();
+        } else if (js_down == 0 && js_right == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkr135:");
+          udp.endPacket();
+        }
+      } else {
+        if (js_up == 0) {
+          if (bt_up == 0) {
+            udp.beginPacket(udpAddress, udpPort);
+            udp.printf(":fastforward:");
+            udp.endPacket();
+          } else {
+            udp.beginPacket(udpAddress, udpPort);
+            udp.printf(":walk0:");
+            udp.endPacket();
+          }
+        } else if (js_down == 0) {
+          if (bt_down == 0) {
+            udp.beginPacket(udpAddress, udpPort);
+            udp.printf(":fastbackward:");
+            udp.endPacket();
+          } else {
+            udp.beginPacket(udpAddress, udpPort);
+            udp.printf(":walk180:");
+            udp.endPacket();
+          }
+        } else if (js_left == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkl90:");
+          udp.endPacket();
+        } else if (js_right == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":walkr90:");
+          udp.endPacket();
+        }
       }
-      else if (js_up == 0 && js_right == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkr45:");
-        udp.endPacket();
-      }
-      else if (js_down == 0 && js_left == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkl135:");
-        udp.endPacket();
-      }
-      else if (js_down == 0 && js_right == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkr135:");
-        udp.endPacket();
-      }
-    }
-    else
-    {
-      if (js_up == 0)
-      {
-        if (bt_up == 0)
-        {
+    } else if ((bt_up + bt_down + bt_left + bt_right + bt_special) != 5) {
+      if (bt_special == 0) {
+        if (bt_up == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":rotatex:");
+          udp.endPacket();
+        } else if (bt_left == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":rotatey:");
+          udp.endPacket();
+        } else if (bt_right == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":rotatez:");
+          udp.endPacket();
+        } else if (bt_down == 0) {
+          udp.beginPacket(udpAddress, udpPort);
+          udp.printf(":twist:");
+          udp.endPacket();
+        }
+      } else if (bt_up == 0) {
+        if (js_up == 0) {
           udp.beginPacket(udpAddress, udpPort);
           udp.printf(":fastforward:");
           udp.endPacket();
@@ -173,10 +230,8 @@ void loop()
           udp.printf(":walk0:");
           udp.endPacket();
         }
-      }
-      else if (js_down == 0)
-      {
-        if (bt_down == 0) {
+      } else if (bt_down == 0) {
+        if (js_down == 0) {
           udp.beginPacket(udpAddress, udpPort);
           udp.printf(":fastbackward:");
           udp.endPacket();
@@ -185,90 +240,26 @@ void loop()
           udp.printf(":walk180:");
           udp.endPacket();
         }
-      }
-      else if (js_left == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkl90:");
-        udp.endPacket();
-      }
-      else if (js_right == 0)
-      {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walkr90:");
-        udp.endPacket();
-      }
-    }
-  }
-  else if ((bt_up + bt_down + bt_left + bt_right + bt_special) != 5)
-  {
-    if (bt_special == 0) {
-      if (bt_up == 0) {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":rotatex:");
-        udp.endPacket();
       } else if (bt_left == 0) {
         udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":rotatey:");
+        udp.printf(":turnleft:");
         udp.endPacket();
       } else if (bt_right == 0) {
         udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":rotatez:");
-        udp.endPacket();
-      } else if (bt_down == 0) {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":twist:");
+        udp.printf(":turnright:");
         udp.endPacket();
       }
-    }
-    else if (bt_up == 0)
-    {
-      if (js_up == 0) {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":fastforward:");
-        udp.endPacket();
-      } else {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walk0:");
-        udp.endPacket();
-      }
-    }
-    else if (bt_down == 0)
-    {
-      if (js_down == 0) {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":fastbackward:");
-        udp.endPacket();
-      } else {
-        udp.beginPacket(udpAddress, udpPort);
-        udp.printf(":walk180:");
-        udp.endPacket();
-      }
-    }
-    else if (bt_left == 0)
-    {
+    } else {
       udp.beginPacket(udpAddress, udpPort);
-      udp.printf(":turnleft:");
+      udp.printf(":standby:");
       udp.endPacket();
     }
-    else if (bt_right == 0)
-    {
-      udp.beginPacket(udpAddress, udpPort);
-      udp.printf(":turnright:");
-      udp.endPacket();
-    }
+    button_pressed--;
   }
-  else
-  {
-    udp.beginPacket(udpAddress, udpPort);
-    udp.printf(":standby:");
-    udp.endPacket();
-  }
-  delay(100);
+  delay(50);
 }
 
-void connectToWiFi(const char *ssid, const char *pwd)
-{
+void connectToWiFi(const char *ssid, const char *pwd) {
   analogWrite(LED_POWER, 2);
   analogWrite(LED_CONNECTED, 0);
 
@@ -279,8 +270,7 @@ void connectToWiFi(const char *ssid, const char *pwd)
   WiFi.onEvent(WiFiEvent);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
     delay(500);
   }
@@ -289,10 +279,8 @@ void connectToWiFi(const char *ssid, const char *pwd)
 }
 
 // WiFi event handler
-void WiFiEvent(WiFiEvent_t event)
-{
-  switch (event)
-  {
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
     case ARDUINO_EVENT_WIFI_STA_GOT_IP:
       // when connected set
       Serial.print("WiFi connected! IP address: ");
